e8bb74555872da54b359b8a5c106b67a76daef65
Mikael Brassman <mishakun@gmail.com>
2013-12-01 14:20:17 +0100

    Simple reveal and count bombs algorithm

--- www/scripts/game/game.js
+++ www/scripts/game/game.js
@@ -1,25 +1,92 @@
 define(['zepto', 'random'], function($, r) {
 
-    var rows = 10, cols = 10, resetGame = function() {
+    var rows = 10, cols = 10, gameTable, resetGame = function() {
         var i;
 
         $('.game-area').children().remove();
+        gameTable = [];
 
         for (i = 0; i < rows; i++) {
-            addRow();
+            gameTable.push(addRow(i));
         }
-    }, addRow = function() {
-        var row = $('<tr></tr>'), i;
+
+        gameTable[4][4].hasBomb = true;
+    }, 
+    addRow = function(yIdx) {
+        var row = $('<tr></tr>'), i, col, datarow = [];
 
         for (i = 0; i < cols; i++) {
-            row.append('<td></td>');
+            col = {
+                e: $('<td data-x="' + i + '" data-y="' + yIdx + '"></td>'),
+                hasBomb: false,
+                x: i,
+                y: yIdx
+            };
+            row.append(col.e);
+            datarow.push(col);
         }
 
         $('.game-area').append(row);
+        return datarow;
+    }, 
+    getNeighbors = function(x, y) {
+        var tileArr = [], i, j, curRow, curCol;
+        for (i = 0; i < 3; i++) {
+            if (i+y-1 < 0 || i+y-1 >= rows) {
+                continue;
+            }
+            curRow = gameTable[i-1+y];
+            for (j = 0; j < 3; j++) {
+                if (i == 1 && j == 1 || j+x-1 < 0 || j+x-1 >= cols) {
+                    continue;
+                } 
+                curCol = curRow[j-1+x];
+                tileArr.push(curCol);
+            }
+        }
+        return tileArr;
+    }, 
+    findBombs = function(x, y) {
+        var bombCount = 0, i, neighbors = getNeighbors(x, y), cur;
+
+        for (i = 0; i < neighbors.length; i++) {
+            cur = neighbors[i];
+            if (cur.hasBomb) {
+                bombCount++;
+            }
+        }
+        return bombCount;
+    }, 
+    reveal = function(x, y) {
+        var bombsFound, i, neighbors, curr, data = gameTable[y][x];
+
+        if(data.e.hasClass('revealed')) {
+            return;
+        }
+        bombsFound = findBombs(x, y);
+
+        data.e.addClass('revealed');
+        data.e.text(bombsFound < 1 ? '' : bombsFound);
+        if (bombsFound < 1) {
+            neighbors = getNeighbors(x, y);
+            for (i = 0; i < neighbors.length; i++) {
+                curr = neighbors[i];
+                reveal(curr.x, curr.y);
+            }
+        }
     };
 
     $(document).on('click', '.game-area td', function() {
-        $(this).addClass('revealed');
+        var $e = $(this),
+            x = $e.data('x'),
+            y = $e.data('y'),
+            data = gameTable[y][x];
+
+        if(data.hasBomb) {
+            $e.addClass('bomb');
+        } else {
+            reveal(x, y);
+        }
     });
 
     $(document).on('click', '.game-reset', function() {
