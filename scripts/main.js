82a6893a3d5294075b9bc6dc733c1161ff6dfe35
Mikael Brassman <mishakun@gmail.com>
2013-12-06 20:27:35 +0100

    Site update

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,20 @@ requirejs.config({
     baseUrl: 'scripts/game',
     paths: {
         'zepto': '../lib/zepto.min',
-        'bootstrap': '../lib/bootstrap.min'
+        'bootstrap': '../lib/bootstrap.min',
+        'knockout': '../lib/knockout-3.0.0'
     },
     shim: {
         'zepto': {
             exports: 'Zepto'
+        },
+        'knockout': {
+            exports: 'ko'
         }
     }
 });
 
-define(['zepto', 'utils', 'game', 'othergames'], function($) {
+define(['utils', 'game'], function() {
+    require(['othergames'], function() {
+    });
 });
