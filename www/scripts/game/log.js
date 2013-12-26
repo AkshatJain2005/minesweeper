01b10896b8f933e8e933abc6f3daeca69ab29924
Mikael Brassman <mishakun@gmail.com>
2013-12-26 21:11:03 +0100

    Fixing some GA events

--- www/scripts/game/log.js
+++ www/scripts/game/log.js
@@ -1,9 +1,13 @@
 define([], function() {
    
-   var exports = {};
+   var exports = {},
+       debug = false || (document.location.hostname == "localhost");
 
    exports.event = function(action, label) {
        ga('send', 'event', 'minesweeper', action, label);
+       if (debug) {
+           console.log('send', 'event', 'minesweeper', action, label);
+       }
    };
 
    return exports;
